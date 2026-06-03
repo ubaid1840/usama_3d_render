5397b297b16e509b027c13594c22ebad5a9514d1
ubaid1840 <ubaidrehman1840@gmail.com>
2026-06-03 14:15:51 +0500

    suspense set

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Suspense } from "react"
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3"
 
 export default function Providers({
@@ -8,10 +9,12 @@ export default function Providers({
   children: React.ReactNode
 }>) {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <GoogleReCaptchaProvider
       reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
     >
       {children}
     </GoogleReCaptchaProvider>
+    </Suspense>
   )
 }
